0c9fa96b0f9a4003152ca5e65cdfa3c492a2c4be
Maqdes <maqdes.ahmad@gmail.com>
2024-09-05 17:50:08 +0300

    Add project files.

---
+++ Models/ViewModel/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Task_Identity.Models.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [MaxLength(40)]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [MaxLength(40)]
+        public string Password { get; set; }
+       
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ Models/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Task_Identity.Models.ViewModel
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [MaxLength(40)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [MaxLength(40)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [MaxLength(40)]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword{ get; set; }
+        public string Phone { get; set; }
+
+
+    }
+}
